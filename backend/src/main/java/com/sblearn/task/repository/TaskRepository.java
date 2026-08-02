c64f59db5e7dd23f837b7daf200e56ff402ea305
James Burge <james3906@gmail.com>
2026-08-02 20:15:39 +0100

    Create task request created

--- backend/src/main/java/com/sblearn/task/repository/TaskRepository.java
+++ backend/src/main/java/com/sblearn/task/repository/TaskRepository.java
@@ -1,5 +1,12 @@
 package com.sblearn.task.repository;
 
-public interface TaskRepository {
+import com.sblearn.task.domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
 
 }
